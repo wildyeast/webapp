bffdf1dee23ef68fe9e8bad9b452d1977c3db7a9
Christoph Döberl <christoph@doeberl.at>
2018-11-29 14:44:44 +0100

    basic download item

--- components/DownloadItem.vue
+++ components/DownloadItem.vue
@@ -1,5 +1,9 @@
 <template>
   <div v-editable="blok" class="download-item">
+    <div class="title">{{blok.name}}</div>
+    <div class="subtitle">
+      {{filename}}
+    </div>
   </div>
 </template>
 
@@ -7,11 +11,26 @@
 export default {
   props: ['blok'],
   created() {
+  },
+  computed: {
+    filename() {
+      let tokens = this.blok.file.split('/');
+      return tokens[tokens.length-1];
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/styles.scss';
+
 .download-item {
+  .title {
+    text-transform: uppercase;
+    color: $color-orange;
+    font-weight: bold;
+  }
+  .subtitle {
+  }
 }
 </style>
