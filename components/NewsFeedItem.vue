84d41063a7ff48d0c3d44b3e7c486465ef2e1ccf
Jonas Karg <jonas.karg@hotmail.com>
2018-12-03 15:53:25 +0100

    Added a month-seperation for the news feed.

--- components/NewsFeedItem.vue
+++ components/NewsFeedItem.vue
@@ -5,7 +5,8 @@
         {{news.source}}
         {{news.datetime}}
       </div>
-      <img :src="news.image" alt=""/>
+
+      <img :src="news.image" alt>
       <h4 class="title">{{news.title}}</h4>
       <span class="text">{{news.text}}</span>
     </div>
@@ -14,32 +15,37 @@
 
 <script>
 export default {
-  props: ['news']
-}
+  props: ["news"]
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/styles.scss';
+@import "@/assets/scss/styles.scss";
 
 .news-feed-item {
   margin-top: 69px;
+  text-align: right;
+
   .container {
-    width: 40%;
     margin: 5rem 2rem 0 2rem;
-    .header {
-      font-weight: bold;
-      padding: 5px 0;
-    }
+
     img {
       width: 100%;
       height: auto;
       display: block;
     }
+
+    .header {
+      font-weight: bold;
+      padding: 5px 0;
+    }
+
     .title {
       margin: 15px 0;
       font-weight: bold;
       font-size: 2rem;
     }
+
     .text {
       font-size: 1rem;
       font-family: $font-mono;
