12b5354f1ea715f9f67431ae823b9234d1892e4f
Christoph Döberl <christoph@doeberl.at>
2018-12-13 10:37:27 +0100

    register function

--- functions/register.js
+++ functions/register.js
@@ -1,1 +1,40 @@
-!function(e,t){for(var n in t)e[n]=t[n]}(exports,function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=14)}({14:function(e,t){t.handler=function(e,t,n){n(null,{statusCode:200,body:'{"success": true}'})}}}));
+const axios = require('axios');
+const baseURL = 'https://fabman.io/api/v1/';
+
+let newMember = {
+  "account": 310,
+  "space": 290,
+  "firstName": "Herbert",
+  "lastName": "Josef",
+  "gender": "female",
+  "dateOfBirth": "2018-12-11",
+  "emailAddress": "test@doebi.at",
+  "company": "string",
+  "phone": "string",
+  "address": "string",
+  "address2": "string",
+  "city": "string",
+  "zip": "string",
+  "countryCode": "AT",
+  "region": "string",
+  "notes": "string",
+  "createdAt": "2018-12-11"
+};
+
+exports.handler = function(event, context, callback) {
+  const instance = axios.create({
+    baseURL,
+    headers: {'Authorization': `Bearer ${process.env.FABMAN_TOKEN}`}
+  });
+
+  instance.post('members', newMember).then((r) => {
+    console.log(r);
+  }).catch((e) => {
+    console.log('ERROR', e);
+  });
+
+  callback(null, {
+    statusCode: 200,
+    body: '{ success: "true" }'
+  });
+};
