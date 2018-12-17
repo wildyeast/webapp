546f5fd0e0b5f5e39e0ef386b83cf09cf45890a6
Christoph Döberl <christoph@doeberl.at>
2018-12-17 13:22:30 +0100

    breadcrumbs

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
     </main>
     <bottom-footer/>
     <sidebar />
+    <breadcrumbs />
   </div>
 </template>
 
@@ -13,12 +14,14 @@
 import TopHeader from '~/components/TopHeader.vue'
 import BottomFooter from '~/components/BottomFooter.vue'
 import Sidebar from '~/components/Sidebar.vue'
+import Breadcrumbs from '~/components/Breadcrumbs.vue'
 
 export default {
   components: {
     TopHeader,
     BottomFooter,
-    Sidebar
+    Sidebar,
+    Breadcrumbs,
   },
 }
 </script>
