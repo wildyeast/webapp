d2066de3da83f4c8ec6b3d2b2aac7dc11da49c7b
Christoph Döberl <christoph@doeberl.at>
2018-12-19 12:05:10 +0100

    temporary lock on de

--- middleware/languageDetection.js
+++ middleware/languageDetection.js
@@ -1,7 +1,11 @@
+const languages = ['de', 'en'];
+const defaultLanguage = 'de';
+
 export default function ({ app, route, store, isDev, redirect }) {
   let version = route.query._storyblok || isDev ? 'draft' : 'published'
-  let language = route.params.language || 'de'
-  let languages = ['de', 'en'];
+  //TODO: redirect if language does not exist
+  //let language = route.params.language || defaultLanguage;
+  let language = defaultLanguage;
 
   if (process.server) {
     store.commit('setCacheVersion', app.$storyapi.cacheVersion)
