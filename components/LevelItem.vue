9665ced2dfe3f073f0b43621c1a619011989a0f9
Jonas Karg <jonas.karg@hotmail.com>
2018-12-26 15:07:52 +0100

    Level slider structure.

--- components/LevelItem.vue
+++ components/LevelItem.vue
@@ -1,14 +1,59 @@
 <template>
-
+  <div class="level-description">
+    <!-- .level-indicator content must be loaded from storyblok language -->
+    <h3 class="level-indicator">Ebene</h3>
+    <h1 class="number">{{number}}</h1>
+    <div class="text">
+      <h2 class="title">{{blok.title}}</h2>
+      <img class="arrow" src="~/assets/img/arrow-right.svg">
+    </div>
+    <h3 class="areas">{{blok.subtitle}}</h3>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["blok", "number"],
+};
 </script>
 
-<style>
+<style lang="scss">
+.level-description {
+  display: flex;
+  margin-bottom: 20px;
 
+  .number {
+    font-size: 80px;
+    margin: 0;
+  }
+
+  .level-indicator {
+    margin: 0;
+    font-size: 14px;
+    border-bottom: 1px solid #000;
+    margin-top: 60px;
+    margin-right: 20px;
+    padding-left: 4px;
+  }
+
+  .areas {
+    font-size: 12px;
+    margin-left: 10px;
+  }
+
+  .text {
+    margin-left: 15px;
+
+    .title {
+      font-size: 18px;
+      margin-top: 5px;
+      margin-bottom: 0;
+    }
+
+    .arrow {
+      width: 180px;
+    }
+  }
+}
 </style>
 
--- components/LevelSlider.vue
+++ components/LevelSlider.vue
@@ -1,16 +1,26 @@
 <template>
-  <h1>
-    {{blok}}
-  </h1>
+  <div class="level-slider">
+    <component
+      v-for="(level, index) in blok.levels"
+      :key="level._uid"
+      :blok="level"
+      :is="level.component"
+      :number="blok.levels.length - 1 - index"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   props: ["blok"]
-}
+};
 </script>
 
-<style>
-
+<style lang="scss">
+  .level-slider {
+    margin: 10px;
+    border: 6px solid #000;
+    padding: 20px 0 10px 0;
+  }
 </style>
 
