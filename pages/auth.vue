caebef06b19ce40a9a0c2c6ded938f3eb3fa756f
Christoph Döberl <christoph@doeberl.at>
2019-01-10 10:39:33 +0100

    fix link

--- pages/auth.vue
+++ pages/auth.vue
@@ -15,7 +15,7 @@ export default {
     if (process.client) {
       let hash = this.$route.hash;
       this.$store.dispatch('auth', { hash }).then((r) => {
-        this.$router.push('me');
+        this.$router.push('/me');
       }).catch((err) => {
         this.$router.push('/');
       });
