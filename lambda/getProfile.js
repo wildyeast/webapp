9c5db5374a784018474251d76af7dd51e49cd599
Christoph Döberl <christoph@doeberl.at>
2019-01-14 16:39:13 +0100

    debugging

--- lambda/getProfile.js
+++ lambda/getProfile.js
@@ -5,15 +5,16 @@ const jwksClient = require('jwks-rsa');
 
 const baseURL = 'https://fabman.io/api/v1/';
 
-// TODO: a hell more of exception handling
+// TODO: a hell more of exception handling and general hardening
 exports.handler = function(event, context, callback) {
+  console.log('a');
   let token = null;
   if (event.headers.cookie) {
     const parsed = cookieparser.parse(event.headers.cookie)
     try {
       token = parsed.jwt
     } catch (err) {
-      console.log(err);
+      //console.log(err);
       return callback(null, {
         statusCode: 401,
         body: 'Unauthorized'
@@ -38,8 +39,11 @@ exports.handler = function(event, context, callback) {
     });
   }
 
+  console.log('b');
+
   jwt.verify(token, getKey, function(err, decoded) {
     if (!err) {
+      console.log('verified');
       let fabmanId = decoded['https://grandgarage.eu/fabmanId'];
 
       console.log('token', process.env.FABMAN_TOKEN);
@@ -55,14 +59,14 @@ exports.handler = function(event, context, callback) {
           body: JSON.stringify(r.data)
         });
       }).catch((e) => {
-        console.log(e);
+        //console.log(e);
         callback(null, {
           statusCode: 500,
           body: 'ERROR'
         });
       });
     } else {
-      console.log(err);
+      //console.log(err);
       callback(null, {
         statusCode: 500,
         body: 'ERROR'
