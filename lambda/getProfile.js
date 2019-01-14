94a062c823ff3aadffe0e4a6b74b2bd7a9e04fda
Christoph Döberl <christoph@doeberl.at>
2019-01-14 16:55:33 +0100

    another try

--- lambda/getProfile.js
+++ lambda/getProfile.js
@@ -7,14 +7,13 @@ const baseURL = 'https://fabman.io/api/v1/';
 
 // TODO: a hell more of exception handling and general hardening
 exports.handler = function(event, context, callback) {
-  console.log('a');
   let token = null;
   if (event.headers.cookie) {
     const parsed = cookieparser.parse(event.headers.cookie)
     try {
       token = parsed.jwt
     } catch (err) {
-      //console.log(err);
+      console.log(err);
       return callback(null, {
         statusCode: 401,
         body: 'Unauthorized'
@@ -39,15 +38,9 @@ exports.handler = function(event, context, callback) {
     });
   }
 
-  console.log('b');
-
   jwt.verify(token, getKey, function(err, decoded) {
     if (!err) {
-      console.log('verified');
       let fabmanId = decoded['https://grandgarage.eu/fabmanId'];
-
-      console.log('keys', Object.keys(process.env));
-      console.log('token', process.env);
 
       const instance = axios.create({
         baseURL,
@@ -60,14 +53,14 @@ exports.handler = function(event, context, callback) {
           body: JSON.stringify(r.data)
         });
       }).catch((e) => {
-        //console.log(e);
+        console.log(e);
         callback(null, {
           statusCode: 500,
           body: 'ERROR'
         });
       });
     } else {
-      //console.log(err);
+      console.log(err);
       callback(null, {
         statusCode: 500,
         body: 'ERROR'
