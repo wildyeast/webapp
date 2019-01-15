f1ad889326d5df53420e22ec7d20912929b3c1d5
Christoph Döberl <christoph@doeberl.at>
2019-01-15 13:11:05 +0100

    try catch

--- lambda/getTrainings.js
+++ lambda/getTrainings.js
@@ -4,22 +4,25 @@ const baseURL = 'https://fabman.io/api/v1/';
 
 exports.handler = function(event, context, callback) {
   let fabmanId = decoded['https://grandgarage.eu/fabmanId'];
-
-  const instance = axios.create({
-    baseURL,
-    headers: {'Authorization': `Bearer ${process.env.FABMAN_TOKEN}`}
-  });
-
-  return instance.get('trainings').then((r) => {
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(r.data)
+  try {
+    const instance = axios.create({
+      baseURL,
+      headers: {'Authorization': `Bearer ${process.env.FABMAN_TOKEN}`}
     });
-  }).catch((e) => {
-    console.log(e);
-    callback(null, {
-      statusCode: 500,
-      body: 'ERROR'
+
+    return instance.get('trainings').then((r) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(r.data)
+      });
+    }).catch((e) => {
+      console.log(e);
+      callback(null, {
+        statusCode: 500,
+        body: 'ERROR'
+      });
     });
-  });
+  } catch(err) {
+    console.log(err);
+  }
 };
