a4e789ffd7b1aef6f2bc6c6f64caf6abc2b4b108
Christoph Döberl <christoph@doeberl.at>
2019-01-17 13:05:13 +0100

    some improvements for level slider

--- components/LevelSlider.vue
+++ components/LevelSlider.vue
@@ -60,13 +60,15 @@ export default {
     overflow: hidden;
 
     .image {
-      width: 100%;
+      max-width: 100%;
+      max-height: 100%;
     }
   }
 }
 
 @media (min-width: $mobile-large) {
   .level-slider {
+    max-height: 75vh;
     display: flex;
     border-right: 6px solid #000;
 
