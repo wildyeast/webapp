8a910d13673f6265bedbb41206c3c2029b9b6693
Christoph Döberl <christoph@doeberl.at>
2019-01-23 22:17:18 +0100

    subtitle in workshop preview

--- components/WorkshopPreview.vue
+++ components/WorkshopPreview.vue
@@ -2,16 +2,12 @@
   <div v-if="story">
     <div class="preview-wrapper">
       <div class="workshop-preview">
-        <nuxt-link v-if="story" class="story" :to="story.full_slug">
+        <nuxt-link class="story" :to="story.full_slug">
           <div class="banner" :style="{ 'background-image': 'url(' + $resizeImage(workshop.image, '700x0') + ')' }"></div>
-          <h3 class="title">{{workshop.title}}</h3>
-          <p class="teaser">{{workshop.teaser}}</p>
+          <div class="title">{{workshop.title}}</div>
+          <div v-if="workshop.subtitle" class="subtitle">{{workshop.subtitle}}</div>
+          <div class="teaser">{{workshop.teaser}}</div>
         </nuxt-link>
-        <div class="loading" v-else>
-          <div class="banner" :style="{ 'background-image': 'url(//placehold.it/700x300)' }"></div>
-          <h3 class="title"></h3>
-          <p class="teaser"></p>
-        </div>
       </div>
     </div>
   </div>
@@ -36,7 +32,6 @@ export default {
     }).then((res) => {
       this.story = res.data.story;
     }).catch((e) => {
-      console.log('error', e);
     });
   },
   methods: {
@@ -81,13 +76,22 @@ export default {
         background-position: center;
       }
 
-      h3.title {
+      .title {
+        margin: 1em 0 0.2em 0;
         font-weight: normal;
         font-size: 1.5rem;
         font-family: $font-secondary;
       }
 
-      p.teaser {
+      .subtitle {
+        color: $color-orange;
+        margin-bottom: 0.5em;
+        font-weight: normal;
+        font-size: 1rem;
+        font-family: $font-mono;
+      }
+
+      .teaser {
         font-size: 1rem;
         font-family: $font-mono;
       }
