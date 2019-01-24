1e07431393496857e629f5170e7113b60f3f81cd
Andreas Demmelbauer <git@notice.at>
2019-01-24 18:39:22 +0100

    minor responsive fixes

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -21,8 +21,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/styles.scss';
+
 .text-image {
-  height: 80vh;
+  min-height: 80vh;
   margin: 0 4%;
   margin-top: 3rem;
   display: flex;
@@ -31,20 +33,34 @@ export default {
     margin-top: 1em;
     font-weight: bold;
     font-size: 3rem;
+    @include media-breakpoint-down(sm) {
+      font-size: 2rem;
+    }
     text-transform: uppercase;
   }
   .body {
     display: flex;
+    @include media-breakpoint-down(md) {
+      flex-direction: column;
+    }
     .text {
       flex: 1;
       display: flex;
       line-height: 1.2;
       font-size: 1.5rem;
+      @include media-breakpoint-down(sm) {
+        font-size: 1rem;
+        line-height: 1.4;
+      }
     }
     .image {
       flex: 1;
       margin-top: 3em;
       padding-left: 3em;
+      @include media-breakpoint-down(md) {
+        margin-top: 1em;
+        padding-left: 0;
+      }
       padding-right: 0;
       img {
         margin: auto;
