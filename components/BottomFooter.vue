5f5cbefb514c40ea73ef6bc17001ce5bfdfed376
Christoph Döberl <christoph@doeberl.at>
2019-01-30 11:58:21 +0100

    name

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -23,7 +23,7 @@
     <div class="background-footer">
       <div class="background-footer-content">
         <div class="newsletter-footer">
-          <form netlify name="newsletter" accept-charset="utf-8">
+          <form netlify accept-charset="utf-8">
             <h4>Immer am Ball bleiben</h4>
             <div class="newsletter-subscribe">
               <input type="email" placeholder="Deine E-Mail Adresse">
