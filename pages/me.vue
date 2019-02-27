ebd4e658b212a2e660ffa22d1c9ca6fb440f8046
Christoph Döberl <christoph@doeberl.at>
2019-02-27 21:13:14 +0100

    reverse profile edit

--- pages/me.vue
+++ pages/me.vue
@@ -4,69 +4,25 @@
       <div class="header">
         <h1 class="name">{{user.profile.firstName}} {{user.profile.lastName}}</h1>
         <code class="number">#{{user.profile.memberNumber}}</code>
-        <span class="email">{{user.profile.emailAddress}}</span>
         <div class="spacer"></div>
         <button @click="logout" class="logout-button">Logout</button>
       </div>
-      <div class="body">
-        <div class="tablist">
-          <ul>
-            <li :class="{ active: activeSection == 'packages' }" @click="setTab('packages')" >Packages</li>
-            <li :class="{ active: activeSection == 'trainings' }" @click="setTab('trainings')">Trainings</li>
-            <li :class="{ active: activeSection == 'profile' }" @click="setTab('profile')">Rechnungsdaten</li>
-          </ul>
+      <div class="section">
+        <h2>Packages</h2>
+        <ul class="item-list" v-if="user.packages && user.packages.length > 0">
+          <li v-for="p in user.packages"><package :userPackage="p" /></li>
+        </ul>
+        <div v-else>
+          <code>Keine Mitgliedschaft abgeschlossen</code>
         </div>
-        <div class="content">
-          <div v-if="activeSection == 'packages'" class="section">
-            <h2>Packages</h2>
-            <ul class="item-list" v-if="user.packages && user.packages.length > 0">
-              <li v-for="p in user.packages"><package :userPackage="p" /></li>
-            </ul>
-            <div v-else>
-              <code>Keine Mitgliedschaft abgeschlossen</code>
-            </div>
-          </div>
-          <div v-if="activeSection == 'trainings'" class="section">
-            <h2>Trainings</h2>
-            <ul class="item-list" v-if="user.trainings && user.trainings.length > 0">
-              <li v-for="t in user.trainings"><training :userTraining="t" /></li>
-            </ul>
-            <div v-else>
-              <code>Noch keine Trainings vorhanden</code>
-            </div>
-          </div>
-          <div v-if="activeSection == 'profile'" class="section">
-            <h2>Rechnungsdaten</h2>
-            <form class="form" @submit.prevent="updateUser">
-              <div class="form-item">
-                <span class="label">Vorname</span>
-                <input class="input-text" disabled type="text" v-model="user.profile.firstName" name="" id=""/>
-              </div>
-              <div class="form-item">
-                <span class="label">Nachname</span>
-                <input class="input-text" disabled type="text" v-model="user.profile.lastName" name="" id=""/>
-              </div>
-              <div class="form-item">
-                <span class="label">Adresse</span>
-                <input class="input-text" type="text" v-model="user.profile.address" name="" id=""/>
-              </div>
-              <div class="form-item">
-                <span></span>
-                <input class="input-text" type="text" v-model="user.profile.address2" name="" id=""/>
-              </div>
-              <div class="form-item">
-                <span class="label">PLZ</span>
-                <input class="input-text" type="text" v-model="user.profile.zip" name="" id=""/>
-              </div>
-              <div class="form-item">
-                <span class="label">Stadt</span>
-                <input class="input-text" type="text" v-model="user.profile.city" name="" id=""/>
-              </div>
-              <div class="button-row">
-                <button type="submit" class="input-button-primary">Speichern</button>
-              </div>
-            </form>
-          </div>
+      </div>
+      <div class="section">
+        <h2>Trainings</h2>
+        <ul class="item-list" v-if="user.trainings && user.trainings.length > 0">
+          <li v-for="t in user.trainings"><training :userTraining="t" /></li>
+        </ul>
+        <div v-else>
+          <code>Noch keine Trainings vorhanden</code>
         </div>
       </div>
     </div>
@@ -77,16 +33,11 @@
 export default {
   middleware: 'authenticated',
   data () {
-    return {
-      activeSection: 'packages'
-    }
+    return {}
   },
   created() {
   },
   methods: {
-    setTab(section) {
-      this.activeSection = section;
-    },
     getPackage(p) {
       let data = this.$store.getters.getPackageById(p.package);
       return { ...p, ...data };
@@ -95,9 +46,6 @@ export default {
       this.$store.dispatch('logout').then(() => {
         this.$router.push('/');
       });
-    },
-    updateUser(event) {
-      this.$store.dispatch('updateUser', Object.assign({}, this.user.profile));
     }
   },
   computed: {
@@ -131,32 +79,6 @@ export default {
       outline: none;
       color: #FFF;
       background-color: $color-orange;
-    }
-  }
-  .body {
-    display: flex;
-    flex-direction: row;
-    .tablist {
-      ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        margin-right: 1em;
-        li {
-          background-color: #FFF;
-          cursor: pointer;
-          user-select: none;
-          padding: 0.5em 1em;
-          &.active {
-            font-weight: bold;
-          }
-        }
-      }
-    }
-    .content {
-      flex: 1;
-      .section {
-      }
     }
   }
   .name {
