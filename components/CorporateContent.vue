e717e13080bf7809507127de75767fd930bd1f49
doebi <christoph@doeberl.at>
2019-07-29 14:46:39 +0200

    added corporate content type

---
+++ components/CorporateContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-editable="blok" class="corporate-content">
+    <div>
+      <div>{{blok.headline}}</div>
+      <div>{{blok.subline}}</div>
+    </div>
+    <div class="text">
+      {{blok.text}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['blok'],
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/styles.scss';
+
+.corporate-content {
+}
+
+</style>
