c3b113df07bc581675cf66b5713ab81082a8a9f2
doebi <christoph@doeberl.at>
2019-08-02 10:07:32 +0200

    basic grand gazette styling

--- components/CorporateContent.vue
+++ components/CorporateContent.vue
@@ -1,11 +1,16 @@
 <template>
   <div v-editable="blok" class="corporate-content">
-    <div>
-      <div>{{blok.headline}}</div>
-      <div>{{blok.subline}}</div>
+    <div class="header">
+      <div class="headline">{{blok.headline}}</div>
+      <div class="subline">{{blok.subline}}</div>
     </div>
-    <div class="text">
-      {{blok.text}}
+    <div class="texter">
+      <div class="text">
+        <markdown :value="blok.text"></markdown>
+      </div>
+      <div class="img">
+        <img :src="blok.image" alt=""/>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +29,37 @@ export default {
 @import '@/assets/scss/styles.scss';
 
 .corporate-content {
+  display: flex;
+  flex-direction: column;
+  margin-left: 4%;
+  margin-right: 4%;
+  .header {
+    padding: 10vh 0;
+    .headline {
+      font-weight: bold;
+      font-size: 10vh;
+    }
+    .subline {
+      color: $color-red;
+      font-size: 5vh;
+    }
+  }
+  .texter {
+    display: flex;
+    flex-direction: row;
+    .text {
+      font-family: $font-secondary;
+      flex: 3;
+    }
+    .img {
+      padding-left: 100px;
+      flex: 2;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
 }
 
 </style>
