29b139b1c7950f4ce621fc9ec05bc5bbc92cec2d
doebi <christoph@doeberl.at>
2019-09-11 05:42:54 +0200

    new profile

--- plugins/components.js
+++ plugins/components.js
@@ -45,6 +45,7 @@ import ImageSlideshow from '~/components/ImageSlideshow.vue'
 import ImageSlideshowBlue from '~/components/ImageSlideshowBlue.vue'
 import SpotlightSlider from '~/components/SpotlightSlider.vue'
 import TextImage from '~/components/TextImage.vue'
+import Text from '~/components/Text.vue'
 
 import LoginForm from '~/components/LoginForm.vue'
 import RegisterForm from '~/components/RegisterForm.vue'
@@ -119,6 +120,7 @@ Vue.component('image-gallery', ImageGallery)
 Vue.component('image-slideshow', ImageSlideshow)
 Vue.component('image-slideshow-blue', ImageSlideshowBlue)
 Vue.component('text-image', TextImage)
+Vue.component('text-blok', Text)
 
 Vue.component('faq-block', FAQBlock)
 Vue.component('faq-item', FAQItem)
