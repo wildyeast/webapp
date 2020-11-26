44f6c8f6353dea152219cc23653e225e77844687
Florian Weinrich <florian@weinrich.dev>
2020-11-26 16:10:56 +0100

    address stuff

--- pages/me/shop.vue
+++ pages/me/shop.vue
@@ -1,24 +1,110 @@
 <template>
   <div>
+
+
+    <component v-if="story.content.component" :key="story.content._uid" :blok="story.content"
+               :is="story.content.component"></component>
+
+
     <div v-if="currentStep == 0">
-      gutscheinverkauf blabla
       <br>
-      <select v-model="selectedProduct">
-        <option value="25">25€</option>
-        <option value="50">50€</option>
-        <option value="100">100€</option>
-        <option value="200">200€</option>
-      </select>
+      <div>
+        <span>
+          Gutschein-Wert:
+        </span>
+        <select v-model="selectedProduct">
+          <option value="69">10€</option>
+          <option value="69">25€</option>
+          <option value="69">50€</option>
+          <option value="69">100€</option>
+        </select>
+      </div>
+
+      <div>
+        <span>
+          Versandoption:
+        </span>
+        <select v-model="selectedExtra">
+          <option value="69">E-mail - Gratis</option>
+          <option value="69">Standard - 3€</option>
+          <option value="69">Deluxe - 20€</option>
+        </select>
+      </div>
+
       <button v-on:click="nextStep()">next</button>
     </div>
     <div v-if="currentStep == 1">
       bestätigung
-      du willst {{ selectedValue }}€ gutschein kaufen
+      du willst {{ selectedProduct }}€ gutschein kaufen
+      delivery method: {{ selectedExtra }}
       <button v-on:click="nextStep()">next</button>
     </div>
     <div v-if="currentStep == 2">
       <input type="radio" name="paymentMethod" value="1" v-model="paymentMethod">Kreditkarte<br>
       <input type="radio" name="paymentMethod" value="2" v-model="paymentMethod">SEPA-Monatsrechnung<br>
+
+
+      <div v-if="invoiceContact != null">
+        <h4>Rechnungsaddresse:</h4>
+
+        <div class="form-item">
+          <span class="label">Vorname</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.firstname" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Nachname</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.lastname" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Telefon</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.phone" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Straße</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.street" name=""/>
+        </div>        <div class="form-item">
+          <span class="label">Straße</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.street_additional" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">PLZ</span>
+          <input class="input-text" type="text" readonly v-model="invoiceContact.zip" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Stadt</span>
+          <input class="input-text" type="text" v-model="invoiceContact.city" name=""/>
+        </div>
+      </div>
+
+      <label for="shippingAddress">Lieferaddresse entspricht nicht der Rechnungsaddresse</label>
+      <input type="checkbox" id="shippingAddress" name="shippingAddress" v-model="shippingAddressEnabled">
+      <div v-if="shippingAddressEnabled">
+        <h4>Lieferaddresse:</h4>
+        <div class="form-item">
+          <span class="label">Vorname</span>
+          <input class="input-text" type="text" v-model="shippingAddress.firstname" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Nachname</span>
+          <input class="input-text" type="text" v-model="shippingAddress.lastname" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Telefon</span>
+          <input class="input-text" type="text" v-model="shippingAddress.phone" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Straße</span>
+          <input class="input-text" type="text" v-model="shippingAddress.address" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">PLZ</span>
+          <input class="input-text" type="text" v-model="shippingAddress.zip" name=""/>
+        </div>
+        <div class="form-item">
+          <span class="label">Stadt</span>
+          <input class="input-text" type="text" v-model="shippingAddress.city" name=""/>
+        </div>
+      </div>
 
       <button v-on:click="checkout()">checkout</button>
     </div>
@@ -46,11 +132,23 @@ export default {
     return {
       currentStep: 0,
       selectedProduct: null,
+      selectedExtra: null,
       paymentMethod: 0,
-      error: ''
+      error: '',
+      shippingAddressEnabled: 0,
+      invoiceContact: null,
+      sepa_active: false,
+      shippingAddress: []
     }
   },
   created() {
+  },
+  mounted() {
+    this.$store.dispatch("getUserMetadata").then(data => {
+      this.invoiceContact = data.data.invoice_contact;
+      this.sepaActive = data.data.sepa_active;
+      console.log(this.userMetadata)
+    })
   },
   methods: {
     nextStep() {
@@ -59,7 +157,13 @@ export default {
     checkout() {
       let data = {
         payment_method: parseInt(this.paymentMethod),
-        productCounts: [{'product_id': 69, 'count': 1}]
+        productCounts: [{
+          'product_id': this.selectedProduct,
+          'count': 1
+        }, {
+          'product_id': this.selectedExtra,
+          'count': 1
+        }]
       };
 
       this.$store.dispatch("checkout", data).then((data) => {
