45c7dcc21ec34698511c9c16891a3b26d9f11ad1
wildyeast <32699708+wildyeast@users.noreply.github.com>
2021-03-11 16:15:31 +0100

    Update covid warnings

--- components/CovidInfobox.vue
+++ components/CovidInfobox.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="covidInfobox">
     <div class="boxContainer">
-      <font-awesome-icon class="icon" icon="exclamation-triangle" />
       <div class="box">
+        <div class="title">
+          COVID-19 INFO ZU DEN WORKSHOPS
+        </div>
         <div class="text">
           Deine Gesundheit ist uns wichtig. Deshalb finden unsere Workshops nur statt, insofern die aktuellen COVID-Maßnahmen der Bundesregierung dies zum Zeitpunkt der Abhaltung erlauben.<br><br>
           Sollte es zu einer Absage kommen, werden dir die Workshop-Kosten selbstverständlich rückerstattet.<br><br>
@@ -20,40 +22,26 @@
   align-items: center;
   width: 100%;
   position: relative;
+  font-family: $font-mono;
   & .boxContainer {
-    & .icon {
-      position: absolute;
-      font-size: 15em;
-      margin-left: 1rem;
-      margin-top: -0.5rem;
-      color: $color-bright-bg;
-      @include media-breakpoint-down(sm) {
-        filter: opacity(0.3);
-        margin-top: -3rem;
-      }
-    }
     & .box {
       display: flex;
       flex-direction: column;
       background-color: $color-yellow;
-      width: 46em;
-      @include media-breakpoint-down(sm) {
-        width: 100%;
-      }
+      width: 100vw;
       min-height: 12.5em;
-      padding: 1em;
-      border: 5px dashed black;
+      padding: 4em 5em;
       margin: 2em;
-      @include media-breakpoint-down(sm) {
-        width: 95vw;
-        margin: 0.3em;
+      & .title {
+        font-size: 1.5em;
+        font-weight: bolder;
       }
       & .text {
-        margin-left: 15em;
-        font-size: 1.1em;
+        margin-top: 1.5em;
+        margin-right: 12em;
+        line-height: 1.3em;
         @include media-breakpoint-down(sm) {
-          margin: 0;
-          z-index: 2;
+          margin-right: 0;
         }
       }
     }
